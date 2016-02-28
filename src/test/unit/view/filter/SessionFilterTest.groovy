a8716cfd7f16927088f61249983da5c9a1376349
Jaume <jaume.jane@fav24.com>
2016-02-28 18:40:20 +0100

    session filter and test

---
+++ src/test/unit/view/filter/SessionFilterTest.groovy
@@ -0,0 +1,80 @@
+package view.filter
+
+import com.sun.net.httpserver.Filter
+import com.sun.net.httpserver.Headers
+import com.sun.net.httpserver.HttpExchange
+import org.apache.http.HttpStatus
+import service.SessionService
+import spock.lang.Specification
+
+class SessionFilterTest extends Specification {
+    private SessionFilter sessionFilter;
+
+    void setup() {
+        sessionFilter = new SessionFilter();
+    }
+
+    def "GetSessionTokenFromCookie"() {
+        expect:
+        sessionToken == sessionFilter.getSessionTokenFromCookie(cookie);
+
+        where:
+        cookie          | sessionToken
+        null            | null
+        "session=token" | "token"
+        "abc=bca"       | null
+
+    }
+
+    def "GetSessionTokenFromCookies"() {
+        expect:
+        sessionToken == sessionFilter.getSessionTokenFromCookies(cookie);
+
+        where:
+        cookie                                    | sessionToken
+        null                                      | null
+        Arrays.asList("session=token")            | "token"
+        Arrays.asList("session=token", "abc=bca") | "token"
+        Arrays.asList("abc=bca")                  | null
+    }
+
+    def "WhenSessionIsValidShouldCallNextChainFilter"() {
+        given:
+        SessionService sessionService = Mock(SessionService.class)
+        sessionService.getUserNameOfSession("token") >> "user"
+        sessionFilter = new SessionFilter(sessionService)
+
+        Headers headers = new Headers();
+        headers.put(sessionFilter.COOKIE_HEADER, Arrays.asList("session=token", "abc=bca"));
+        HttpExchange t = Mock(HttpExchange.class);
+        t.getRequestHeaders() >> headers
+
+        Filter.Chain chain = Mock(Filter.Chain.class);
+        when:
+        sessionFilter.doFilter(t, chain);
+        then:
+        1 * chain.doFilter(_)
+    }
+
+    def "WhenSessionIsInValidShouldNotCallNextChainFilterAndShouldRedirect"() {
+        given:
+        SessionService sessionService = Mock(SessionService.class)
+        sessionService.getUserNameOfSession("token") >> null
+        sessionFilter = new SessionFilter(sessionService)
+
+        Headers headers = new Headers();
+        headers.put(sessionFilter.COOKIE_HEADER, Arrays.asList("session=token", "abc=bca"));
+        HttpExchange t = Mock(HttpExchange.class);
+        t.getRequestHeaders() >> headers
+        t.getResponseHeaders() >> headers
+
+        Filter.Chain chain = Mock(Filter.Chain.class);
+        when:
+        sessionFilter.doFilter(t, chain);
+        then:
+        0 * chain.doFilter(_)
+        1 * t.sendResponseHeaders(HttpStatus.SC_MOVED_TEMPORARILY, -1L);
+    }
+
+
+}
